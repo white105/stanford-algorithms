24ed4a342e697bd8d27bc84852841eb0865174a3
Nick <white105@my.canisius.edu>
2017-11-08 17:36:47 -0500

    implement with range loop

--- merge_sort.py
+++ merge_sort.py
@@ -37,9 +37,43 @@ def mergeSort(unsorted_array):
 
         return unsorted_array
 
+def merge_sort(unsorted_array):
+
+    if(len(unsorted_array) == 0 or len(unsorted_array) == 1):
+        return unsorted_array
+    else:
+        midpoint = len(unsorted_array) // 2
+        left_half = unsorted_array[:midpoint]
+        right_half = unsorted_array[midpoint:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+
+        for k in range(0, len(unsorted_array)):
+            if (i < len(left_half) and j < len(right_half)):
+                if (left_half[i] < right_half[j]):
+                    unsorted_array[k] = left_half[i]
+                    i += 1
+                else:
+                    unsorted_array[k] = right_half[j]
+                    j += 1
+            else:
+                if (i < len(left_half)):
+                    unsorted_array[k] = left_half[i]
+                    i += 1
+
+                if (j < len(right_half)):
+                    unsorted_array[k] = right_half[j]
+                    j += 1
+
+        return unsorted_array
+
 
 unsorted_array = [4, 3, 5, 2, 1, 7, 8, 6]
 #unsorted_array = [2, 1]
 print('unsorted array :', unsorted_array)
-sorted_array = mergeSort(unsorted_array)
+sorted_array = merge_sort(unsorted_array)
 print('sorted array :', sorted_array)
